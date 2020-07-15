8dd3a16160e772eb492e7d414c494c5a61a5fb99
Philip Hamner <philiphamner@gmail.com>
2020-07-14 17:34:32 -0700

    second deck shuffle project, setup

---
+++ shuffle-deck-two.js
@@ -0,0 +1,23 @@
+// Let’s implement a deck of playing cards in an object-oriented way with JavaScript. 
+// Then we’ll add methods for additional functionality such as 
+// a shuffle operation, a reset operation, and a deal operation that will return a random card and remove it from the deck.
+
+
+class Deck {
+    constructor(){
+        this.deck = [];
+
+        const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
+        const values = ['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'];
+
+
+        for (var i = 0; i < suits.length; i++) {
+            for (var j = 0; j < values.length; j++) {
+                this.deck.push(`${values[j]} of ${suits[i]}`)
+            }
+        }
+    }
+    display(){
+        return this.deck;
+    }
+}
