4ec15c475e3e3ae2638c0499f65474b57eec820a
Philip Hamner <philiphamner@gmail.com>
2020-07-14 18:11:19 -0700

    working solution.

--- fuzzy-palindrones.js
+++ fuzzy-palindrones.js
@@ -5,7 +5,11 @@
 // racecar === racecaR
 
 let checkIfFuzzyPalindrones = function(word1, word2){
-    return word1.toUpperCase();
+    let upperCaseWord1 = word1.toUpperCase();
+    let upperCaseWord2 = word2.toUpperCase();
+    let reversedWord1 = upperCaseWord1.split('').reverse().join('');
+
+    return reversedWord1 === upperCaseWord2;
 }
 
-console.log(checkIfFuzzyPalindrones('hello', 'world'));
+console.log(checkIfFuzzyPalindrones('Racecar', 'racecar'));
