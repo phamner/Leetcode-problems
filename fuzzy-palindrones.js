0eb47ca8f5b6f2820060c09b9d6f2bc85cf401bc
Philip Hamner <philiphamner@gmail.com>
2020-07-14 18:08:17 -0700

    getting problem set up.

---
+++ fuzzy-palindrones.js
@@ -0,0 +1,11 @@
+// Write a function that returns whether or not two strings are fuzzy palindromes of one another 
+// (i.e. ignore spaces and capitalization).  
+
+
+// racecar === racecaR
+
+let checkIfFuzzyPalindrones = function(word1, word2){
+    return word1.toUpperCase();
+}
+
+console.log(checkIfFuzzyPalindrones('hello', 'world'));
