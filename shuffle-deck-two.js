6120d987fc27fa22f23bd74303997e50bd8e1a8d
Philip Hamner <philiphamner@gmail.com>
2020-07-14 17:46:00 -0700

    add shuffle and deal functionality.

--- shuffle-deck-two.js
+++ shuffle-deck-two.js
@@ -7,21 +7,44 @@ class Deck {
     constructor(){
         this.deck = [];
 
-        const suits = ['hearts', 'clubs', 'diamonds', 'spades'];
-        const values = ['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'];
+        this.suits = ['hearts', 'clubs', 'diamonds', 'spades'];
+        this.values = ['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'];
 
 
-        for (var i = 0; i < suits.length; i++) {
-            for (var j = 0; j < values.length; j++) {
-                this.deck.push(`${values[j]} of ${suits[i]}`)
+        for (var i = 0; i < this.suits.length; i++) {
+            for (var j = 0; j < this.values.length; j++) {
+                this.deck.push(`${this.values[j]} of ${this.suits[i]}`)
             }
         }
     }
     display(){
         return this.deck;
     }
+    shuffle(){
+        const { deck } = this;
+        let m = deck.length, i;
+      
+        while (m) {
+          i = Math.floor(Math.random() * m--);
+      
+          [deck[m], deck[i]] = [deck[i], deck[m]];
+        }
+      
+        return this;
+    }
+    reset(){
+        this.deck = [];
+        for (var i = 0; i < this.suits.length; i++) {
+            for (var j = 0; j < this.values.length; j++) {
+                this.deck.push(`${this.values[j]} of ${this.suits[i]}`)
+            }
+        }
+    }
+    deal(){
+        return this.deck.pop();
+    }
 }
 
 const freshyDeck = new Deck();
-
-console.log(freshyDeck.deck)
+freshyDeck.deal();
+console.log(freshyDeck.deck.length)
